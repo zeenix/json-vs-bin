0c711502aae7ecf17395448df49bf452e7d6daf3
Zeeshan Ali Khan <zeenix@gmail.com>
2024-02-21 13:18:08 +0100

    Rename stuff for the future & split some common code

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -10,57 +10,41 @@ use serde::{Deserialize, Serialize};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use serde_json::to_string;
+use serde_json::{to_string, to_vec};
 use zvariant::{serialized::Context, to_bytes_for_signature, Type, LE};
 
-criterion_group!(benches, dbus_enc, json_enc, bson_enc, cbor_enc);
+criterion_group!(benches, dbus, json, bson, cbor);
 criterion_main!(benches);
 
-fn dbus_enc(c: &mut Criterion) {
-    let data = iter::repeat_with(Data::new).take(10).collect::<Vec<_>>();
-    let signature = <Vec<Data>>::signature();
+fn dbus(c: &mut Criterion) {
+    let data = iter::repeat_with(BigData::new).take(10).collect::<Vec<_>>();
+    let signature = <Vec<BigData>>::signature();
     let ctxt = Context::new_dbus(LE, 0);
 
-    c.bench_function("dbus_enc_no_context_switching", |b| {
+    c.bench_function("dbus_big_no_context_switching", |b| {
         b.iter(|| {
             let encoded =
                 to_bytes_for_signature(black_box(ctxt), black_box(&signature), black_box(&data))
                     .unwrap();
 
-            let (data, _): (Vec<Data>, _) = encoded
+            let (data, _): (Vec<BigData>, _) = encoded
                 .deserialize_for_signature(black_box(&signature))
                 .unwrap();
             black_box(data);
         })
     });
 
-    // Create 8 threads and channels, with main thread receiving back what it sends to the first
-    // channel, from the last channel in the chain.
-    let (first_tx, mut last_rx) = channel();
-    for _ in 0..available_parallelism()
-        .map(Into::into)
-        .unwrap_or(DEFAULT_PARALLELISM)
-    {
-        let (tx, mut rx) = channel();
-        swap(&mut last_rx, &mut rx);
-        std::thread::spawn(move || loop {
-            let msg = match rx.recv() {
-                Ok(msg) => msg,
-                Err(_) => break,
-            };
-            tx.send(msg).unwrap();
-        });
-    }
+    let (first_tx, last_rx) = setup_channels_and_threads();
 
-    c.bench_function("dbus_enc_high_context_switching", |b| {
+    c.bench_function("dbus_big_high_context_switching", |b| {
         b.iter(|| {
             let encoded =
                 to_bytes_for_signature(black_box(ctxt), black_box(&signature), black_box(&data))
                     .unwrap();
-            first_tx.send(encoded).unwrap();
+            first_tx.send(encoded.to_vec()).unwrap();
 
-            let encoded = last_rx.recv().unwrap();
-            let (data, _): (Vec<Data>, _) = encoded
+            let encoded = zvariant::serialized::Data::new(last_rx.recv().unwrap(), ctxt);
+            let (data, _): (Vec<BigData>, _) = encoded
                 .deserialize_for_signature(black_box(&signature))
                 .unwrap();
             black_box(data);
@@ -68,57 +52,41 @@ fn dbus_enc(c: &mut Criterion) {
     });
 }
 
-fn json_enc(c: &mut Criterion) {
-    let data = iter::repeat_with(Data::new).take(10).collect::<Vec<_>>();
+fn json(c: &mut Criterion) {
+    let data = iter::repeat_with(BigData::new).take(10).collect::<Vec<_>>();
 
-    c.bench_function("json_enc_no_context_switching", |b| {
+    c.bench_function("json_big_no_context_switching", |b| {
         b.iter(|| {
             let encoded = to_string(black_box(&data)).unwrap();
-            let data: Vec<Data> = serde_json::from_slice(encoded.as_bytes()).unwrap();
+            let data: Vec<BigData> = serde_json::from_slice(encoded.as_bytes()).unwrap();
             black_box(data);
         })
     });
 
-    // Create 8 threads and channels, with main thread receiving back what it sends to the first
-    // channel, from the last channel in the chain.
-    let (first_tx, mut last_rx) = channel();
-    for _ in 0..available_parallelism()
-        .map(Into::into)
-        .unwrap_or(DEFAULT_PARALLELISM)
-    {
-        let (tx, mut rx) = channel();
-        swap(&mut last_rx, &mut rx);
-        std::thread::spawn(move || loop {
-            let msg = match rx.recv() {
-                Ok(msg) => msg,
-                Err(_) => break,
-            };
-            tx.send(msg).unwrap();
-        });
-    }
+    let (first_tx, last_rx) = setup_channels_and_threads();
 
-    c.bench_function("json_enc_high_context_switching", |b| {
+    c.bench_function("json_big_high_context_switching", |b| {
         b.iter(|| {
-            let encoded = to_string(black_box(&data)).unwrap();
+            let encoded = to_vec(black_box(&data)).unwrap();
             first_tx.send(encoded).unwrap();
 
             let encoded = last_rx.recv().unwrap();
-            let data: Vec<Data> = serde_json::from_slice(encoded.as_bytes()).unwrap();
+            let data: Vec<BigData> = serde_json::from_slice(&encoded).unwrap();
             black_box(data);
         })
     });
 }
 
-fn bson_enc(c: &mut Criterion) {
-    let data = iter::repeat_with(Data::new).take(10).collect::<Vec<_>>();
+fn bson(c: &mut Criterion) {
+    let data = iter::repeat_with(BigData::new).take(10).collect::<Vec<_>>();
     // BSON can't handle arrays at the top level, so we wrap it in a struct.
     #[derive(Deserialize, Serialize, Type, PartialEq, Debug, Clone)]
     struct Foo {
-        data: Vec<Data>,
+        data: Vec<BigData>,
     }
     let data = Foo { data };
 
-    c.bench_function("bson_enc_no_context_switching", |b| {
+    c.bench_function("bson_big_no_context_switching", |b| {
         b.iter(|| {
             let encoded = bson::to_vec(black_box(&data)).unwrap();
             let data: Foo = bson::from_slice(&encoded).unwrap();
@@ -126,25 +94,9 @@ fn bson_enc(c: &mut Criterion) {
         })
     });
 
-    // Create 8 threads and channels, with main thread receiving back what it sends to the first
-    // channel, from the last channel in the chain.
-    let (first_tx, mut last_rx) = channel();
-    for _ in 0..available_parallelism()
-        .map(Into::into)
-        .unwrap_or(DEFAULT_PARALLELISM)
-    {
-        let (tx, mut rx) = channel();
-        swap(&mut last_rx, &mut rx);
-        std::thread::spawn(move || loop {
-            let msg = match rx.recv() {
-                Ok(msg) => msg,
-                Err(_) => break,
-            };
-            tx.send(msg).unwrap();
-        });
-    }
+    let (first_tx, last_rx) = setup_channels_and_threads();
 
-    c.bench_function("bson_enc_high_context_switching", |b| {
+    c.bench_function("bson_big_high_context_switching", |b| {
         b.iter(|| {
             let encoded = bson::to_vec(black_box(&data)).unwrap();
             first_tx.send(encoded).unwrap();
@@ -156,51 +108,35 @@ fn bson_enc(c: &mut Criterion) {
     });
 }
 
-fn cbor_enc(c: &mut Criterion) {
-    let data = iter::repeat_with(Data::new).take(10).collect::<Vec<_>>();
+fn cbor(c: &mut Criterion) {
+    let data = iter::repeat_with(BigData::new).take(10).collect::<Vec<_>>();
 
-    c.bench_function("cbor_enc_no_context_switching", |b| {
+    c.bench_function("cbor_big_no_context_switching", |b| {
         b.iter(|| {
             let mut encoded = Vec::new();
             ciborium::into_writer(black_box(&data), black_box(&mut encoded)).unwrap();
-            let data: Vec<Data> = ciborium::from_reader(black_box(&encoded[..])).unwrap();
+            let data: Vec<BigData> = ciborium::from_reader(black_box(&encoded[..])).unwrap();
             black_box(data);
         })
     });
 
-    // Create 8 threads and channels, with main thread receiving back what it sends to the first
-    // channel, from the last channel in the chain.
-    let (first_tx, mut last_rx) = channel();
-    for _ in 0..available_parallelism()
-        .map(Into::into)
-        .unwrap_or(DEFAULT_PARALLELISM)
-    {
-        let (tx, mut rx) = channel();
-        swap(&mut last_rx, &mut rx);
-        std::thread::spawn(move || loop {
-            let msg = match rx.recv() {
-                Ok(msg) => msg,
-                Err(_) => break,
-            };
-            tx.send(msg).unwrap();
-        });
-    }
+    let (first_tx, last_rx) = setup_channels_and_threads();
 
-    c.bench_function("cbor_enc_high_context_switching", |b| {
+    c.bench_function("cbor_big_high_context_switching", |b| {
         b.iter(|| {
             let mut encoded = Vec::new();
             ciborium::into_writer(black_box(&data), black_box(&mut encoded)).unwrap();
             first_tx.send(encoded).unwrap();
 
             let encoded = last_rx.recv().unwrap();
-            let data: Vec<Data> = ciborium::from_reader(black_box(&encoded[..])).unwrap();
+            let data: Vec<BigData> = ciborium::from_reader(black_box(&encoded[..])).unwrap();
             black_box(data);
         })
     });
 }
 
 #[derive(Deserialize, Serialize, Type, PartialEq, Debug, Clone)]
-struct Data {
+struct BigData {
     int1_loooooooooooooooooooooooooooooooooooong_name: u64,
     int2_loooooooooooooooooooooooooooooooooooooong_name: u8,
     bool1_looooooooooooooooooooooong_name: bool,
@@ -232,7 +168,7 @@ struct Data {
     string8: String,
     map5_loooooooooooooooooooooooong_name: std::collections::HashMap<String, u32>,
 }
-impl Data {
+impl BigData {
     pub fn new() -> Self {
         let string = iter::repeat('o').take(250).collect::<String>();
         let mut map = HashMap::new();
@@ -273,6 +209,31 @@ impl Data {
             map5_loooooooooooooooooooooooong_name: map,
         }
     }
+}
+
+fn setup_channels_and_threads() -> (
+    std::sync::mpsc::Sender<Vec<u8>>,
+    std::sync::mpsc::Receiver<Vec<u8>>,
+) {
+    // Create 8 threads and channels, with main thread receiving back what it sends to the first
+    // channel, from the last channel in the chain.
+    let (first_tx, mut last_rx) = channel();
+    for _ in 0..available_parallelism()
+        .map(Into::into)
+        .unwrap_or(DEFAULT_PARALLELISM)
+    {
+        let (tx, mut rx) = channel();
+        swap(&mut last_rx, &mut rx);
+        std::thread::spawn(move || loop {
+            let msg = match rx.recv() {
+                Ok(msg) => msg,
+                Err(_) => break,
+            };
+            tx.send(msg).unwrap();
+        });
+    }
+
+    (first_tx, last_rx)
 }
 
 const DEFAULT_PARALLELISM: usize = 8;
